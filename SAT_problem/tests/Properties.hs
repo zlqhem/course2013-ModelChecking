b0451a90709c7a566cbbcf2894fa1659c0f96f9d
junhee cho <zlqhem@daum.net>
2013-07-29 00:14:26 +0900

    fixes problem of exhausted test data before falsification by reducing the range of test data

--- SAT_problem/tests/Properties.hs
+++ SAT_problem/tests/Properties.hs
@@ -4,19 +4,34 @@ import Test.QuickCheck
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import GenCNF_Queen
 
-propDiagonal :: Int -> (Int, Int) -> (Int, Int) -> Property
-propDiagonal w p1@(x,y) p2@(x2, y2) =
-  isDiagonal p1 p2 ==> 
+newtype SmallPoint = SmallPoint (Int, Int) deriving Show
+instance Arbitrary SmallPoint where
+  arbitrary = do
+    x <- elements [1..5]
+    y <- elements [1..5]
+    return $ SmallPoint (x, y)
+
+newtype Board = Board Int deriving Show
+instance Arbitrary Board where
+  arbitrary = do
+    w <- elements [5..7]
+    return $ Board w
+
+propDiagonal :: Board -> SmallPoint -> SmallPoint -> Property
+propDiagonal (Board w) (SmallPoint (x,y)) (SmallPoint (x2, y2)) =
+  let p1 = (x,y) in
+  let p2 = (x2, y2) in
   inRange w p1 ==> 
-  inRange w p2 ==> elem (p1,p2) constraints || elem (p2,p1) constraints
-  where constraints = noMoreThanOneQueenInDiagonal2 w
+  inRange w p2 ==> case isDiagonal p1 p2 of
+    True -> elem (p1,p2) constraints || elem (p2,p1) constraints
+    False -> not $ elem (p1,p2) constraints || elem (p2,p1) constraints
+  where constraints = noMoreThanOneQueenInDiagonal w
 
 isDiagonal (x,y) (x2, y2) = x /= x2 && (slope == 1 || slope == -1)
   where slope = fromIntegral (y2-y) / fromIntegral (x2-x)
 
 inRange w (x,y) = x >= 1 && x <= w &&
-                  y >= 1 && y <= w &&
-                  w >= 1 && w <= 30
+                  y >= 1 && y <= w 
 
 --properties :: [Test]
 properties =
